7f2c18a282a9e9f6763c56338b39b772fbd00b25
Glecio Santos <glecio.santos@gmail.com>
2023-02-02 13:12:58 -0300

    Next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,16 +2,18 @@ import styles from './styles.module.scss'
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import Image from 'next/image'
+import { signIn, signOut, useSession} from 'next-auth/client'
  
 export function SignInButton() {
 
-    const session = true;
+    const [session] = useSession();
 
+    console.log(session)
     return session ?(
         <button 
             type='button'
             className={styles.signInButton}
-            onClick={ () => {}}
+            onClick={ () => signOut()}
         >
         <Image src="/images/steve.png" alt="Steve" width={200} height={200} />
         Olá, Steve
@@ -22,7 +24,7 @@ export function SignInButton() {
         <button
             type='button'
             className={styles.signInButton}
-            onClick={ () => {}}
+            onClick={ () => signIn('github')}
         >
             <FaGithub color='FFB800'/>
             Entrar com Github
--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash } from 'react-icons/fi'
+import { SupportButton } from '../../components/SupportButton'
 import styles from './styles.module.scss'
 
 export default function Board() {
@@ -53,6 +54,7 @@ export default function Board() {
                     </time>
                 </div>
             </div>
+            <SupportButton/>
         
         </>
         )

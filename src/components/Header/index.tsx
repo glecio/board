9344955aa0e810a7f142ee75642c3e3a2df26ee2
Glecio Santos <glecio.santos@gmail.com>
2023-02-02 10:23:12 -0300

    Criação do Header e estilos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import styles from './styles.module.scss'
+import { SignInButton } from '../SignInButton'
+
 export function Header() {
     return(
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className={styles.headerContainer}>
+            <div className={styles.herderContent}>
+                <Link href="/">
+                    <img src="/images/logo.svg" alt="Logo Board" />
+                </Link>
+                <nav>
+                    <Link href="/">Home</Link>
+                    <Link href="/board">Meu  Board</Link>
+                </nav>
+                <SignInButton/>
+            </div>
+        </header>
     )
 }
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,30 @@
+import styles from './styles.module.scss'
+import {FaGithub} from 'react-icons/fa'
+import {FiX} from 'react-icons/fi'
+ 
+export function SignInButton() {
+
+    const session = true;
+
+    return session ?(
+        <button 
+            type='button'
+            className={styles.signInButton}
+            onClick={ () => {}}
+        >
+        <img src="https://sujeitoprogramador.com/steve.png" alt="Steve" />
+        Olá, Steve
+        <FiX color='737380' className={styles.closeIcon} />
+        </button>
+    ) :
+    (
+        <button
+            type='button'
+            className={styles.signInButton}
+            onClick={ () => {}}
+        >
+            <FaGithub color='FFB800'/>
+            Entrar com Github
+        </button>
+    )
+}
